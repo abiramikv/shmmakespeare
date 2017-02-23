064b7c1ad55d6d7d18816f5160a7bbed1fbd1d91
Gavy Aggarwal <gavyaggarwal@gmail.com>
2017-02-23 03:08:24 -0800

    Split IO + fixed state bug

--- hmm.py
+++ hmm.py
@@ -1,51 +1,64 @@
 import numpy as np
-
 import warnings
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore",category=DeprecationWarning)
     from hmmlearn import hmm
 
-dictionary = []
-dictMap = {}
-i = 0
+import utils
 
-def dictionaryAdd(w):
-    global i
-    w = w.lower().strip(",.")
-    if w not in dictMap:
-        dictMap[w] = i
-        dictionary.append(w)
-        i += 1
-    return dictMap[w]
-
-X = []
-lengths = []
-
-def processLine(line):
-    global X, maxLength
-    if len(line) < 30:
-        return
-    tokens = [dictionaryAdd(w) for w in line.split()]
-    X += tokens
-    lengths.append(len(tokens))
-
-with open("shakespeare.txt", 'r') as f:
-    for line in f:
-        processLine(line)
-
-with open("spenser.txt", 'r') as f:
-    for line in f:
-        processLine(line)
+dictionary, X, lengths = utils.parseInput()
 
 trainData = np.array([X]).reshape(-1, 1)
-model = hmm.GaussianHMM(n_components=10, n_iter=20, verbose=True)
+model = hmm.MultinomialHMM(n_components=10, n_iter=3, verbose=True)
 model.fit(trainData, lengths=lengths)
+
+def printSonnet(sonnet):
+    for line in sonnet:
+        words = [w for (w, _) in line]
+        print ' '.join(words)
+    print ' '
+
+def generateNextWord((prevWord, prevState)):
+    if not prevState:
+        X, Z = model.sample(max(lengths))
+        return (dictionary[X[0][0]], Z[0])
+    else:
+        transitions = model.transmat_[prevState]
+        nextState = np.random.choice(len(transitions), p=transitions)
+        nextWord = model._generate_sample_from_state(nextState)
+        return (dictionary[nextWord[0]], nextState)
+
+def checkLine(sonnet, currLine, line):
+    if len(currLine) > 8:
+        return "finished"
+    else:
+        return "valid"
+
+def makeSonnet(model):
+    sonnet = []
+    currLine = []
+    word = (None, None)
+    line = 1
+    while line <= 14:
+        printSonnet(sonnet)
+        nextWord = generateNextWord(word)
+        currLine.append(nextWord)
+        lineStatus = checkLine(sonnet, currLine, line)
+        if lineStatus == "invalid":
+            word = currLine.pop()
+        elif lineStatus == "finished":
+            sonnet.append(currLine)
+            currLine = []
+            line += 1
+    printSonnet(sonnet)
+
+makeSonnet(model)
 
 sonnet = []
 for _ in range(14):
     X, Z = model.sample(max(lengths))
-    sonnet.append(' '.join([dictionary[i] for i in Z]))
+    sonnet.append(' '.join([dictionary[i[0]] for i in X]))
 
 for line in sonnet:
     print line
---
+++ utils.py
@@ -0,0 +1,32 @@
+def parseInput():
+    dictionary = []
+    dictMap = {}
+
+    def dictionaryAdd(w):
+        w = w.lower().strip(",.()'")
+        if w not in dictMap:
+            i = len(dictionary)
+            dictMap[w] = i
+            dictionary.append(w)
+        return dictMap[w]
+
+    X = []
+    lengths = []
+
+    def processLine(line):
+        if len(line) < 30:
+            return
+        words = line.split()
+        for word in words:
+            X.append(dictionaryAdd(word))
+        lengths.append(len(words))
+
+    with open("shakespeare.txt", 'r') as f:
+        for line in f:
+            processLine(line)
+
+    with open("spenser.txt", 'r') as f:
+        for line in f:
+            processLine(line)
+
+    return dictionary, X, lengths
