c80bf8e02fdb5cb6f18c478de0712ec6eaa5a27f
Abirami <abi1kur11ven22@gmail.com>
2017-02-23 03:10:22 -0800

    helper functions for syllables and rhyme

---
+++ lang.py
@@ -0,0 +1,42 @@
+from nltk.corpus import cmudict
+
+words = cmudict.dict()
+
+def numSyllables(word):
+    count = 0
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+    # first attempt to use NLTK database
+    if word in words:
+
+        count = len(list(phoneme for phoneme in words[word][0] if phoneme[-1].isdigit()))
+
+    # otherwise use own algorithm if word not found in cmudict
+    else:
+        if word[0] in vowels:
+            count += 1
+        for i in range(len(word) - 1):
+            if word[i] not in vowels and word[i+1] in vowels:
+                count += 1
+        if word[-1] == 'e':
+            count -= 1
+        if count == 0:
+            count += 1
+
+    return count
+    
+# returns true if words rhyme, false if they do not or they are not found in dictionary
+def checkRhyme(word1, word2, level):
+    if word1 in words and word2 in words:
+        phon1 = words[word1]
+        phon2 = words[word2]
+
+        if min(len(phon1[0]), len(phon2)) < level:
+            level = min(len(phon1[0]), len(phon2))
+
+        for version1 in phon1:
+            for version2 in phon2:
+                if version1[-level:] == version2[-level:]:
+                    print(version1[-level:], version2[-level:])
+                    return True
+    return False
